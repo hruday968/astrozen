167e30cf7761272c916f71f6e15b1139c9054c1b
Pragna <pragna.katasani@gmail.com>
2026-01-12 02:17:35 +0530

    Added changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,21 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Hruday Pabbisetty — Software Engineer",
+  author: "Hruday Pabbisetty",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Software Engineer based in Tempe, Arizona, USA.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/hp.jpg",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "Instagram", href: "https://www.instagram.com/hrudaypabbisetty/" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/hruday-pabbisetty/" },
+    { text: "Github", href: "https://github.com/hruday968" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,41 +23,46 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Hruday Pabbisetty",
+    specialty: "Software Engineer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Software Engineer based in Tempe, Arizona, USA.",
+    email: "hrudaipabbisetty@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
+      company: "Microsoft",
+      position: "Software Engineer II",
+      startDate: "Jun 2019",
+      endDate: "Nov 2024",
+      summary: [
+        "Led cross-functional efforts to engineer an automated Kubernetes pruning system that deleted 6,000+ orphaned resources across clusters, slashing security risks and compute costs by millions, while boosting deployment success rate by 2%",
+        "Architected a real-time diagnostics platform using Kubernetes Pod Events API to proactively detect deployment anomalies. Reduced incident triage time by 80% and improved deployment success from 95% to 97%.",
+        "Built and deployed an internal tool for Skype for Business (SFB) that cut onboarding setup time by 95.8%—from 12 hours to 30 minutes—saving support teams hours per customer and streamlining activation across the US Navy.",
+        "Designed and launched a real-time security microservice that automatically mitigated active threats on on-prem servers without requiring software patches. Prevented compromise of thousands of enterprise servers and enabled instant containment with zero downtime, even in legacy environments.",
+        " Developed dual-channel push notification architecture for Skype for Business to ensure iOS 13 compliance, enabling 100% reliable delivery of both VoIP and Non-VoIP messages through Apple Push Notification Service (APNS) and ensuring uninterrupted communication.",
+        "Engineered a high-impact HTTP security filter that blocked 100% of malicious web requests, preventing CVE-grade exploits in Skype for Business, widely adopted across teams as a standard for securing web endpoints."
+      ],
+    },
+    {
+      company: "Amazon",
+      position: "Software Development Intern",
       startDate: "May 2018",
-      endDate: "Sept 2020",
+      endDate: "Jul 2018",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Spearheaded the development of scheduling APIs that replaced a 6-hour manual vendor workflow with real-time automation, enhancing reliability and vendor satisfaction.",
+        "Built a DynamoDB validation layer to detect data mismatches post-migration, fixing critical sync issues that previously caused system failures. Reduced production discrepancies by 90%, significantly improving migration confidence and rollout stability.",
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
+      company: "CodeChef",
+      position: "DSA Problem Curator (Freelance)",
+      startDate: "Jul 2017",
+      endDate: "Jan 2020",
       summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+        "Created and published algorithmic problems with well-defined constraints, edge cases, and test data for CodeChef programming contests.",
+        "Reviewed and validated peer submissions, ensuring clarity, correctness, and alignment with contest standards."
       ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
     },
   ],
   projects: [
@@ -87,11 +90,15 @@ export const SITE_CONTENT: SiteContent = {
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+      Hi, I’m Hruday Pabbisetty, a backend-focused Software Engineer with over 5 years of experience building large-scale distributed systems and cloud infrastructure at Microsoft.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+My work centers around designing reliable services, automating complex operational workflows, and improving system resiliency in Kubernetes-based environments. I’ve built platforms for orphan-resource cleanup, deployment failure diagnostics, and security mitigation that operate at production scale and directly impact system reliability and cost efficiency.
+
+With a strong foundation in algorithms and system design — including a LeetCode rating of 2424 (Top 0.1%) and a Top-30 finish at ACM-ICPC Asia Regionals — I bring a problem-solving mindset to every layer of the backend stack. I’m currently pursuing my Master’s in Software Engineering at Arizona State University, continuing to deepen my expertise in distributed systems and scalable backend architectures.
+
+I’m passionate about Kubernetes, cloud platforms, and building backend systems that engineers trust at scale.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/hp.jpg",
   },
 };
 
